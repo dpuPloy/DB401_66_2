2f5c7d9c7be69ec97c7d7dcf15bd063419c12042
dpuPloy <65112462@dpu.ac.th>
2024-02-21 11:40:28 +0700

    updateReport

--- weathercast/lib/Report.dart
+++ weathercast/lib/Report.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:geocoding/geocoding.dart';
-import 'location.dart';
 import 'forecast.dart';
+import 'weather.dart';
 
 class Report extends StatefulWidget {
   const Report({super.key});
@@ -11,13 +10,27 @@ class Report extends StatefulWidget {
 }
 
 class _ReportState extends State<Report> {
+  Weather? _weather;
+
+  void updateReport() {
+    forecast().then((weather) {
+      setState(() {
+        _weather = weather;
+        ScaffoldMessenger.of(context).removeCurrentSnackBar();
+      });
+    }).catchError((error) {
+      ScaffoldMessenger.of(context).removeCurrentSnackBar();
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text(error.toString()),
+        duration: const Duration(days: 1),
+      ));
+    });
+  }
+
   @override
   void initState() {
-    forecast().then((v) => print(v.address));
-    //getCurrentLocation().then((location) =>
-    //    placemarkFromCoordinates(location.latitude, location.longitude)
-    //        .then((placemarks) => print(placemarks.first)));
     super.initState();
+    updateReport();
   }
 
   @override
@@ -31,13 +44,47 @@ class _ReportState extends State<Report> {
               fontSize: 36, fontWeight: FontWeight.bold, color: Colors.white),
         ),
         Container(
-          constraints: const BoxConstraints.tightFor(width: 150, height: 150),
+          constraints: _weather == null
+              ? const BoxConstraints.tightFor(width: 150, height: 150)
+              : null,
+          padding: const EdgeInsets.all(20),
           decoration: BoxDecoration(
               color: Colors.blueAccent.shade700.withOpacity(0.7),
               borderRadius: BorderRadius.circular(10)),
           margin: const EdgeInsets.symmetric(vertical: 30),
+          child: _weather == null
+              ? null
+              : Column(children: [
+                  Text(
+                    _weather!.address,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.bodyLarge,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    '${_weather!.temperature}℃',
+                    style: Theme.of(context).textTheme.headlineLarge,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    _weather!.condition,
+                    style: Theme.of(context).textTheme.displayMedium,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    _weather!.symbol,
+                    style: const TextStyle(fontSize: 72),
+                  ),
+                ]),
         ),
-        ElevatedButton(onPressed: () {}, child: const Text('Refresh'))
+        FilledButton(
+            onPressed: () => updateReport(), child: const Text('Refresh'))
       ],
     );
   }
